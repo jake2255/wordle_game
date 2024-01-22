3d3eb666c337990da51864204a86fd9072cb24f9
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-01-21 21:44:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+#include "wordbank.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
 
-    cout <<"####WORDLE####"<< endl;
+    cout <<"####WORDLE GAME####"<< endl;
+    cout << endl;
+
+    cout << "Guess a 5-letter word:" << endl;
+    // while(){
+    //     cout << "| X | X | X | X | X |" << endl;
+    // }
+
+    // srand(time(NULL));
+    
 
     return 0;
 }
