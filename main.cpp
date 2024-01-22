8a029727df2a3f5cf7fd671f2c8333256527e20d
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-01-21 20:26:41 -0600

    Added files for main function and wordbank

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout <<"####WORDLE####"<< endl;
+
+    return 0;
+}
---
+++ wordbank.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string WordBank(){
+    
+    return 0;
+}
