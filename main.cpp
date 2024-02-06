419d2b6aad848fad6c0670c1748f5283b95f3a0d
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-02-05 22:08:52 -0600

    Update main.cpp
    Now does the basic function of the wordle game, still requires checks and further control

--- main.cpp
+++ main.cpp
@@ -4,16 +4,32 @@ using namespace std;
 
 int main(){
 
-    cout <<"####WORDLE GAME####"<< endl;
-    cout << endl;
+    vector<char>genWord = WordGenerator();
+    vector<vector<char>>dispWord = {{'*','*','*','*','*'},{genWord[0],genWord[1],genWord[2],genWord[3],genWord[4]}};
+    string word;
+    bool match = false;
 
-    cout << "Guess a 5-letter word:" << endl;
-    // while(){
-    //     cout << "| X | X | X | X | X |" << endl;
-    // }
+    for(int i = 0; i < 7; i++){
+        cout << "guess word: ";
+        cin >> word;
+        cout << endl;
 
-    // srand(time(NULL));
-    
+        vector<char>userWord(word.begin(), word.end());
+
+        for(int i = 0; i < 5; i++){
+            if(dispWord[1][i] == userWord[i])
+                cout << "|" << dispWord[1][i];
+            else
+                cout << "|" << dispWord[0][i];   
+        }
+        cout << "|" << endl;
+    }
+
+    cout << "the word was: " << endl;
+    for(int i = 0; i < 5; i++){
+        cout << "|" << dispWord[1][i];  
+    }
+    cout << "|" << endl;
 
     return 0;
 }
