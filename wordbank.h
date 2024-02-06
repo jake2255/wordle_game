fac62bdf02e615742d54660c5e57ef9bb0d562bb
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-02-05 20:37:25 -0600

    Update and rename wordbank.h to wordgenerator.h
    Added some tags

--- wordbank.h
+++
@@ -1,8 +0,0 @@
-#ifndef WORDBANK_H
-#define WORDBANK_H
-
-#include <string>
-
-std::string WordBank();
-
-#endif
---
+++ wordgenerator.h
@@ -0,0 +1,13 @@
+#ifndef WORDGENERATOR_H
+#define WORDGENERATOR_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+std::vector<char> WordGenerator();
+
+#endif
